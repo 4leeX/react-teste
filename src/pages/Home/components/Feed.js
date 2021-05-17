c71c3b3c0390d0e50c45c29a671c356fba686bc0
4leex <alexsandro.sousa12366@gmail.com>
2021-05-17 17:01:00 -0300

    components

--- src/pages/Home/components/Feed.js
+++ src/pages/Home/components/Feed.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import PostCard from "../../../components/PostCard";
+
+const useStyles = makeStyles((theme) => {
+  root: {
+  }
+});
+
+const posts = [
+  { id: 1, description: "teste 1" },
+  { id: 2, description: "teste 2" },
+];
 
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <div>
+      {posts.map((post) => (
+        <PostCard key={post.id} />
+      ))}
+    </div>
+  );
 }
 
 export default Feed;
