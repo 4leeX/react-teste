78b836256dbb0e274009bb9c27c96354b4c99a01
Alexsandro <alaexsandro.sousa1236@gmail.com>
2021-05-17 11:17:33 -0300

    First commit

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+const useStyles = makeStyles({});
+
+function Header() {
+  return (
+    <AppBar position="fixed" color="inherit">
+      <Toolbar>
+        {/* <div>
+          <a href="/">Conecta Dev - Alterado</a>
+          <input type="text" />
+        </div>
+        <div>
+          <Button color="primary" variant="contained">
+            Novo Post
+          </Button>
+          <span>img1</span>
+          <span>img2</span>
+        </div> */}
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+// import "./style.css";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Feed from "./components/Feed";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  main: {
+    height: "100vh",
+    display: "flex",
+    width: "1200px",
+    margin: "0 auto",
+  },
+});
+
+function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Header />
+      <div className="toolbar"></div>
+      <main className={classes.main}>
+        <NavBar />
+        <Feed />
+      </main>
+    </div>
+  );
+}
+
+export default Home;
