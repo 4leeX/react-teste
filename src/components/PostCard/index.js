88e5fc4d02b8e6e0bac26031eb27340ef0d7e098
4leex <alexsandro.sousa12366@gmail.com>
2021-05-17 21:01:43 -0300

    posts

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function PostCard({ post }) {
-  return <h1>Post</h1>;
+  return <h1>{post.description}</h1>;
 }
 
 export default PostCard;
--- src/pages/Home/components/Feed.js
+++ src/pages/Home/components/Feed.js
@@ -8,15 +8,44 @@ const useStyles = makeStyles((theme) => {
 });
 
 const posts = [
-  { id: 1, description: "teste 1" },
-  { id: 2, description: "teste 2" },
+  {
+    id: 1,
+    autor: {
+      id: 1,
+      name: "Lucas Nhimi",
+      username: "lucasnhimi",
+      avatar: "/images/avatars/avatar_1.jpeg",
+    },
+    title: "Criando um App do zero utilizando React.js",
+    date: "April 7, 2020",
+    description: "Fala pessoal! Qual o framework favorito de vcs?",
+    hashtags: "#dotnet, #javascript, #reactjs, #developer",
+    image: "/images/posts/post9.jpeg",
+  },
+  {
+    id: 2,
+    autor: {
+      id: 1,
+      name: "Lucas Nhimi",
+      username: "lucasnhimi",
+      avatar: "/images/avatars/avatar_1.jpeg",
+    },
+    title: "Comparativo entre React.js e Vue.js - Performance",
+    date: "April 1, 2020",
+    description:
+      "Quero criar um bootcamp gratuito para passar um pouco da minha experiência para vcs! Quem topa?",
+    hashtags: "#dotnet, #javascript, #reactjs, #developer",
+    image: "/images/posts/post8.jpeg",
+  },
 ];
 
 function Feed() {
+  const classes = useStyles();
+
   return (
-    <div>
+    <div className={classes.root}>
       {posts.map((post) => (
-        <PostCard key={post.id} />
+        <PostCard key={post.id} post={post} />
       ))}
     </div>
   );
