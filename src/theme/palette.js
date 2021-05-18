2bfd8c32d697ca1882059f6d0ec16fcee354b102
4leex <alexsandro.sousa12366@gmail.com>
2021-05-18 16:54:34 -0300

    typography

---
+++ src/theme/palette.js
@@ -0,0 +1,10 @@
+import { colors } from "@material-ui/core";
+
+export default {
+  primary: {
+    main: colors.deepPurple["A400"],
+  },
+  secondary: {
+    main: colors.deepPurple["A100"],
+  },
+};
