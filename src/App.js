4d4448b704e5d65065b9aa76c95345cc69e9c22d
4leex <alexsandro.sousa12366@gmail.com>
2021-05-19 10:15:46 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Home from "./pages/Home";
 import { ThemeProvider } from "@material-ui/core";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
 import theme from "./theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
