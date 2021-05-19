f4d206b54967d2fbe2dbbe436e7025c8aa261c1d
4leex <alexsandro.sousa12366@gmail.com>
2021-05-19 10:59:02 -0300

    sign-in style

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,44 @@
 import React from "react";
 
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "row",
+    height: "100vh",
+  },
+  left: {
+    background: "blue",
+    flexBasis: "58%",
+
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  right: {
+    background: "purple",
+    flexBasis: "42%",
+  },
+}));
+
 function SignIn() {
-  return <h1>Login Component</h1>;
+  const classes = useStyles();
+
+  return (
+    // Flex Container
+    <div className={classes.root}>
+      {/* Flex item container */}
+      <div className={classes.left}>
+        <h2>Título</h2>
+        <h4>SubTitulo</h4>
+      </div>
+
+      {/* Flex item */}
+      <div className={classes.right}>Form</div>
+    </div>
+  );
 }
 
 export default SignIn;
