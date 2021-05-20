24db06104e14f280678fdba6af7c74432b763124
4leex <alexsandro.sousa12366@gmail.com>
2021-05-20 10:22:57 -0300

    novo css

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -6,11 +6,11 @@ import Box from "@material-ui/core/Box";
 import Avatar from "@material-ui/core/Avatar";
 import LockOutlined from "@material-ui/icons/LockOutlined";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    // display: "flex",
-    // flexDirection: "row",
     height: "100vh",
   },
 
@@ -19,31 +19,32 @@ const useStyles = makeStyles((theme) => ({
     backgroundPosition: "center",
     backgroundSize: "cover",
     backgroundRepeat: "none",
+    padding: theme.spacing(2),
+    textAlign: "center",
   },
   avatar: {
     background: theme.palette.primary.main,
     marginBottom: theme.spacing(1),
   },
-  // left: {
-  //   background: "blue",
-  //   flexBasis: "58%",
-
-  //   display: "flex",
-  //   flexDirection: "column",
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  // },
-  // right: {
-  //   background: "purple",
-  //   flexBasis: "42%",
-  // },
-  // form: {
-  //   display: "flex",
-  //   flexDirection: "column",
-  //   margin: "65px 32px",
-  //   alignItems: "center",
-  // },
+  button: {
+    marginTop: theme.spacing(1),
+  },
+  form: {
+    margin: theme.spacing(2, 4),
+  },
 }));
+
+function Copyright() {
+  return (
+    <Typography variant="body2" align="center">
+      {"Copyright © "}
+      <a color="inherif" href="#">
+        Alexsandro Ricardo
+      </a>{" "}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
 
 function SignIn() {
   const classes = useStyles();
@@ -83,42 +84,45 @@ function SignIn() {
             <LockOutlined />
           </Avatar>
           <Typography variant="h5">Acesso</Typography>
+          <form className={classes.form}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="E-mail"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Entrar
+            </Button>
+            <Grid item>
+              <Link>Esqueceu a sua senha?</Link> <Link>Não tem uma conta?</Link>
+            </Grid>
+          </form>
+          <Copyright />
         </Box>
       </Grid>
     </Grid>
-
-    //   // Flex Container
-    //   <div className={classes.root}>
-    //     {/* Flex item container */}
-    //     <div className={classes.left}>
-    //       <Typography style={{ color: "#fff", fontSize: 25, lineHeight: "45px" }}>
-    //         <strong>
-    //           Simplificando a forma de conectar desenvolvedores de software!
-    //         </strong>
-    //       </Typography>
-    //       <Typography
-    //         variant="body2"
-    //         style={{
-    //           color: "rgb(255,255,255, 0.7)",
-    //           marginTop: 30,
-    //           fontSize: 15,
-    //           lineHeight: "30px",
-    //         }}
-    //       >
-    //         Compartilhe seu conhecimento com toda nossa rede de desenvolvedores de
-    //         software.
-    //       </Typography>
-    //     </div>
-
-    //     {/* Flex item */}
-    //     <div className={classes.right}>
-    //       <form className={classes.form}>
-    //         <h4>Acesso</h4>
-    //         <input type="text" />
-    //         <input type="text" />
-    //       </form>
-    //     </div>
-    //   </div>
   );
 }
 
