1b776c16a0f6b266cb473fb065546f1402478514
4leex <alexsandro.sousa12366@gmail.com>
2021-05-30 14:02:42 -0300

    refactoring

--- src/pages/Home/components/Header.js
+++ src/pages/Home/components/Header.js
@@ -32,8 +32,7 @@ const useStyles = makeStyles({
 
 function Header() {
   const classes = useStyles();
-  const user = useSelector((state) => state.user);
-  console.log(user);
+  const account = useSelector((state) => state.account);
 
   return (
     <AppBar position="fixed" color="inherit" className={classes.appBar}>
@@ -51,7 +50,7 @@ function Header() {
           <SvgIcon className={classes.bell}>
             <Bell />
           </SvgIcon>
-          <Avatar alt="Remy Sharp" src={user && user.avatar} />
+          <Avatar alt="Remy Sharp" src={account.user && account.user.avatar} />
         </div>
       </Toolbar>
     </AppBar>
