455dd927d55862b52c73124de36daea84e79ccde
4leex <alexsandro.sousa12366@gmail.com>
2021-06-05 13:45:19 -0300

    Menu

--- src/pages/Home/components/Header/Account.js
+++ src/pages/Home/components/Header/Account.js
@@ -1,13 +1,45 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Avatar from "@material-ui/core/Avatar";
+import React, { useState, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import Avatar from '@material-ui/core/Avatar';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 function Account() {
   const account = useSelector((state) => state.account);
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef();
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleSignOut = () => {
+    handleClose();
+  };
 
   return (
     <>
-      <Avatar alt="Remy Sharp" src={account.user && account.user.avatar} />
+      <Avatar ref={ref} onClick={handleOpen} alt="Remy Sharp" src={account.user && account.user.avatar} />
+      <Menu
+        anchorEl={ref.current}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        open={isOpen}
+        onClose={handleClose}
+        getContentAnchorEl={null}
+      >
+        <MenuItem>Perfil</MenuItem>
+        <MenuItem>Meus Favoritos</MenuItem>
+        <MenuItem>Meus Posts</MenuItem>
+        <MenuItem>Minhas Conexões</MenuItem>
+        <MenuItem onClick={handleSignOut}>Sair</MenuItem>
+      </Menu>
     </>
   );
 }
