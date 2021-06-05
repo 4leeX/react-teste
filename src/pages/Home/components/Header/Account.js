19e91eb60907e1278cf39090033d0de009da4d5f
4leex <alexsandro.sousa12366@gmail.com>
2021-06-05 13:59:03 -0300

    Redirecionando SingOut

--- src/pages/Home/components/Header/Account.js
+++ src/pages/Home/components/Header/Account.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -11,6 +12,9 @@ function Account() {
   const [isOpen, setIsOpen] = useState(false);
   const ref = useRef();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const isAuthenticated = !!account.user;
 
   const handleOpen = () => {
     setIsOpen(true);
@@ -24,27 +28,44 @@ function Account() {
     handleClose();
 
     dispatch(signOut());
+    navigate('/');
   };
 
   return (
     <>
       <Avatar ref={ref} onClick={handleOpen} alt="Remy Sharp" src={account.user && account.user.avatar} />
-      <Menu
-        anchorEl={ref.current}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        open={isOpen}
-        onClose={handleClose}
-        getContentAnchorEl={null}
-      >
-        <MenuItem>Perfil</MenuItem>
-        <MenuItem>Meus Favoritos</MenuItem>
-        <MenuItem>Meus Posts</MenuItem>
-        <MenuItem>Minhas Conexões</MenuItem>
-        <MenuItem onClick={handleSignOut}>Sair</MenuItem>
-      </Menu>
+      {isAuthenticated ? (
+        <Menu
+          anchorEl={ref.current}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          open={isOpen}
+          onClose={handleClose}
+          getContentAnchorEl={null}
+        >
+          <MenuItem>Perfil</MenuItem>
+          <MenuItem>Meus Favoritos</MenuItem>
+          <MenuItem>Meus Posts</MenuItem>
+          <MenuItem>Minhas Conexões</MenuItem>
+          <MenuItem onClick={handleSignOut}>Sair</MenuItem>
+        </Menu>
+      ) : (
+        <Menu
+          anchorEl={ref.current}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          open={isOpen}
+          onClose={handleClose}
+          getContentAnchorEl={null}
+        >
+          <MenuItem>Registrar</MenuItem>
+          <MenuItem>Entrar</MenuItem>
+        </Menu>
+      )}
     </>
   );
 }
