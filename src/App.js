23a791cde5be3889c3fb1b1250cad392b1c26091
4leex <alexsandro.sousa12366@gmail.com>
2021-06-08 17:05:11 -0300

    add Novo Post

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { ThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Provider } from "react-redux";
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-import Auth from "./components/Auth";
-import GuestRoute from "./routes/GuestRoute";
-import SignIn from "./pages/SignIn";
-import Home from "./pages/Home";
-import theme from "./theme";
-import store from "./store";
-
-import "./mock";
+import Auth from './components/Auth';
+import GuestRoute from './routes/GuestRoute';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import theme from './theme';
+import store from './store';
+import NewPost from './pages/Post/New';
+import './mock';
 
 function App() {
   return (
@@ -20,6 +20,7 @@ function App() {
           <Auth>
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/post/new" element={<NewPost />} />
               <GuestRoute path="/sign-in" element={<SignIn />} />
               <Route path="*" element={<h1>Not Found 404!</h1>} />
             </Routes>
